527f3fa377d0b6ef439953f8787e67c77908cd23
yq <770144167@qq.com>
2019-01-25 15:00:02 +0800

    5555

--- ConsoleApp1/ConsoleApp1/Person.cs
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -9,7 +9,6 @@ namespace ConsoleApp1
     class Person
     {
 
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
